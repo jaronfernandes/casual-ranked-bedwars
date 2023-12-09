884b394b0383de199e1a0b129a3a8c068fd046cc
fern1394 <fernandesjaron@gmail.com>
2023-12-08 21:59:18 -0500

    Got basic slash commands working

--- main.py
+++ main.py
@@ -12,9 +12,34 @@ intents = discord.Intents.all()
 # intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+## FUNCTIONS
+
+
+## EVENTS
+
+@bot.tree.command(name = "commandname", description = "My first application Command")
+async def first_command(interaction):
+    await interaction.response.send_message("Hello!")
+
 @bot.event
 async def on_ready():
+    await bot.tree.sync()
     print('Bot is ready.')
 
-bot.login(BOT_TOKEN)
-# bot.run(BOT_TOKEN)
+    
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('play'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('oof'):
+        await message.channel.send('Bye!')
+
+@bot.event
+async def end():
+    await bot.close()
+
+bot.run(BOT_TOKEN)
