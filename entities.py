b74a83169ac41784d1be6188bff9e2b9b3e56e7c
fern1394 <fernandesjaron@gmail.com>
2024-01-03 05:07:52 -0500

    Added TeamMakeEmbed to entiteis for match making.

--- entities.py
+++ entities.py
@@ -28,7 +28,7 @@ class Match:
     host: discord.User
     max_players: int = 2
     players: dict[discord.User: int]
-    teams: dict[str: list[str]]
+    teams: dict[str: list[discord.User]]
     map: str
     players_in_game: dict[discord.User: bool]
     is_big_team_map: bool
@@ -60,7 +60,7 @@ class Match:
 
     def get_players(self) -> dict[discord.User: int]:
         """Return the players in the match."""
-        return [player for player in self.players]
+        return self.players
     
     def get_players_and_elo(self) -> dict[discord.User: int]:
         """Return the players along with their ELO in the match."""
@@ -98,6 +98,10 @@ class Match:
         del self.players_in_game[player]
         del self.players[player]
 
+    def is_full(self) -> bool:
+        """Return whether the match is full."""
+        return len(self.players) == self.max_players
+
     def set_teams(self, teams: dict[str, list[str]]) -> None:
         """Set the teams of the match."""
         self.teams = teams
@@ -132,7 +136,7 @@ class MatchMakeEmbed():
         self.title = f"New Casual Ranked Bedwars Match {match.id}"
         self.match = match
         self.img_file = None
-        self.players = self.match.get_players()
+        self.players = self.match.get_players().keys()
         self.embed = discord.Embed(
             title=self.title, 
             description=str(match),
@@ -158,6 +162,91 @@ class MatchMakeEmbed():
             "Max Players: " + str(self.match.get_max_players()) + "\n" 
 
         self.embed.add_field(name="Settings", value=settings_str, inline=True)
+
+    def get_embed(self) -> discord.Embed:
+        """Return the embed."""
+        return self.embed
+    
+    def get_file(self) -> discord.File:
+        """Return the file."""
+        return self.img_file
+    
+    def get_host(self) -> discord.User:
+        """Return the host."""
+        return self.match.get_host()
+    
+    def _get_players_string(self) -> str:
+        """Return a string of players."""
+        plrs_str = ""
+        
+        for player in self.players:
+            plrs_str += "<@"+ str(player.id) + ">, "
+
+        plrs_str = plrs_str.removesuffix(", ")
+
+        return plrs_str
+    
+    def set_teams(self, teams: dict[str, list[str]]) -> None:
+        """Set the teams of the match."""
+        self.teams = teams
+
+
+class TeamMakeEmbed():
+    match: Match
+    title: str
+    players: list[discord.User]
+    img_file: discord.File
+    embed: discord.Embed
+
+    def __init__(self, match: Match):
+        super().__init__()
+        self.title = f"New Casual Ranked Bedwars Match {match.id}"
+        self.match = match
+        self.img_file = None
+        self.players = self.match.get_players()
+        self.embed = discord.Embed(
+            title=self.title, 
+            description=str(match),
+            color=0x00ff00
+        )
+
+        self.embed.add_field(name="Players (" + str(len(self.players)) + "/" + str(self.match.get_max_players()) + ")", value=self._get_players_string(), inline=True)
+
+        try:
+            file = discord.File("map_images/"+match.map+".png", filename=match.map+".png")
+            self.embed.set_image(url="attachment://" + file.filename)
+            self.img_file = file
+            print('embed set')
+        except:
+            print("No image found for map "+match.map+", using default instead.")
+            file = discord.File("map_images/hypixel.png", filename="hypixel.png")
+            self.embed.set_image(url="attachment://" + file.filename)
+            self.img_file = file
+
+        # settings_str = \
+        #     "Randomized Teams: " + ("Enabled" if self.match.get_randomized_teams() else "Disabled") + "\n" + \
+        #     "Randomized Captains: " + ("Enabled" if self.match.get_randomized_captains() else "Disabled") + "\n" + \
+        #     "Max Players: " + str(self.match.get_max_players()) + "\n" 
+            
+        captains_str = \
+            "Team One Captain: <@" + str(self.match.teams["Team One"][0].id) + ">\n" + \
+            "Team Two Captain: <@" + str(self.match.teams["Team Two"][0].id) + ">\n"
+            
+        team_one_str, team_two_str = "", ""
+        
+        for player in self.match.teams["Team One"]:
+            team_one_str += "<@"+ str(player.id) + ">, "
+
+        for player in self.match.teams["Team Two"]:
+            team_two_str += "<@"+ str(player.id) + ">, "
+
+        team_one_str = team_one_str.removesuffix(", ")
+        team_two_str = team_two_str.removesuffix(", ")
+
+        self.embed.add_field(name="Captains", value=captains_str, inline=False)
+        self.embed.add_field(name="Team 1", value=team_one_str, inline=True)
+        self.embed.add_field(name="Team 2", value=team_two_str, inline=True)
+
 
     def get_embed(self) -> discord.Embed:
         """Return the embed."""
